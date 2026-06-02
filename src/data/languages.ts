11a33151adfc0a62daffd9b4ddcaea4bd9b09481
CWARDCSU <93813970+CWARDCSU@users.noreply.github.com>
2026-06-01 18:56:12 -0600

    More features and bug fixes
    Hoping to deploy on github this time

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,7 +1,7 @@
 export const languages = [
-  { name: "Python",     proficiency: 3, icon: "logos:python" },
+  { name: "Python",     proficiency: 4, icon: "logos:python" },
   { name: "TypeScript", proficiency: 1, icon: "skill-icons:typescript" },
-  { name: "Astro",      proficiency: 1, icon: "devicon:astro" },
+  { name: "Astro",      proficiency: 1, icon: "skill-icons:astro" },
   { name: "HTML",       proficiency: 1, icon: "skill-icons:html" },
   { name: "CSS",        proficiency: 1, icon: "devicon:css" },
 ];
--- src/data/libraries.ts
+++ src/data/libraries.ts
@@ -1,8 +1,8 @@
 export const libraries = [
-  { name: "NumPy",        proficiency: 3, icon: "logos:numpy" },
+  { name: "NumPy",        proficiency: 4, icon: "logos:numpy" },
   { name: "Pandas",       proficiency: 3, icon: "devicon:pandas" },
   { name: "Matplotlib",   proficiency: 3, icon: "devicon:matplotlib" },
-  { name: "Scikit-learn", proficiency: 1, icon: "devicon:scikitlearn" },
+  { name: "Scikit-learn", proficiency: 1, icon: "devicon:scikitlearn"},
   { name: "SciPy",        proficiency: 3, icon: "simple-icons:scipy" },
   { name: "PyTorch",      proficiency: 1, icon: "devicon:pytorch" },
   { name: "Plotly",       proficiency: 2, icon: "devicon:plotly" },
